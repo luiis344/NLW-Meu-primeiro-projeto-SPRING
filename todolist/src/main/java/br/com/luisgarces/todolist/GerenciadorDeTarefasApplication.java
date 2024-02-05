f24849cc6539c676180c802c6f0bf695da9065a3
Luis <luisgarcesaraujo@gamil.com>
2024-02-05 10:23:05 -0300

    Novas alterações simples

---
+++ todolist/src/main/java/br/com/luisgarces/todolist/GerenciadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.luisgarces.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeTarefasApplication.class, args);
+	}
+
+}
